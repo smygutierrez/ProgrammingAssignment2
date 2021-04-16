ee2c7a9b0f0c29fe7da3372d5fc0575d274bd658
smygutierrez <iyagutz@gmail.com>
2021-04-16 23:15:39 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,65 @@
 ## These functions create a matrx and caches the inverse of the matrix.
 ## makeCacheMatrix function creates a matrix that caches its inverse
 
-makeCacheMatrix
-function(x = matrix()) {
-inv <- NULL
-set <- function(y) {
-x <<- y
-inv <<- NULL
-}
-get <- function() x
-         setInverse <- function(inverse) inv <<- inverse
-         getInverse <- function() inv
-         list(set = set,
-              get = get,
-              setInverse = setInverse,
-              getInverse = getInverse)
-}
+specialmatrix<- makeCacheMatrix(matrix(1:16,4,4))
+> cacheSolve(specialmatrix)
+Error in solve.default(mat, ...) : 
+  Lapack routine dgesv: system is exactly singular: U[3,3] = 0
+> specialmatrix$get()
+     [,1] [,2] [,3] [,4]
+[1,]    1    5    9   13
+[2,]    2    6   10   14
+[3,]    3    7   11   15
+[4,]    4    8   12   16
+> specialmatrix$getInverse()
+NULL
+> cacheSolve(specialmatrix)
+Error in solve.default(mat, ...) : 
+  Lapack routine dgesv: system is exactly singular: U[3,3] = 0
+> specialmatrix$getInverse()
+NULL
+> specialmatrix$get()
+     [,1] [,2] [,3] [,4]
+[1,]    1    5    9   13
+[2,]    2    6   10   14
+[3,]    3    7   11   15
+[4,]    4    8   12   16
+> specialmatrix$setInverse()
+Error in specialmatrix$setInverse() : 
+  argument "inverse" is missing, with no default
+> specialmatrix$getInverse()
+NULL
 
 
 ## cacheSolve is a funtion that computes the inverse of the generated matrix of funtion makeCacheMatrix.
 
-cacheSolve
-function(x, ...) {
-inv <- x$getInverse()
-         if (!is.null(inv)) {
-                 message("getting cached data")
-                 return(inv)
-         }
-         mat <- x$get()
-         inv <- solve(mat, ...)
-         x$setInverse(inv)
+specialmatrix$get()
+     [,1] [,2] [,3] [,4]
+[1,]    1    5    9   13
+[2,]    2    6   10   14
+[3,]    3    7   11   15
+[4,]    4    8   12   16
+> specialmatrix$setInverse()
+Error in specialmatrix$setInverse() : 
+  argument "inverse" is missing, with no default
+> specialmatrix$getInverse()
+NULL
+> specialmatrix$set(matrix(16:1,4,4))
+> specialmatrix$get()
+     [,1] [,2] [,3] [,4]
+[1,]   16   12    8    4
+[2,]   15   11    7    3
+[3,]   14   10    6    2
+[4,]   13    9    5    1
+> specialmatrix$getInverse()
+NULL
+> cacheSolve(specialmatrix)
+Error in solve.default(mat, ...) : 
+  Lapack routine dgesv: system is exactly singular: U[3,3] = 0
+> cacheSolve(specialmatrix)
+Error in solve.default(mat, ...) : 
+  Lapack routine dgesv: system is exactly singular: U[3,3] = 0
+> specialmatrix$getInverse()
+NULL
 
-        ## Return a matrix that is the inverse of 'x'
-inv
-}
+For some reason my test keeps coming out this way?
